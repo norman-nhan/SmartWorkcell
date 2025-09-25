9a988dc5b2fc155c4ab7a589b560199acd8e1a5f
Tang Phu Thien Nhan <tangptnhan@gmail.com>
2025-09-25 17:14:23 +0900

    better result from PCA

--- utils.py
+++ utils.py
@@ -22,7 +22,6 @@ def realsense_setup():
     pipeline = rs.pipeline()
     config = rs.config()
     colorizer = rs.colorizer()
-    # config.enable_device("816513022078")
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     return pipeline, config, colorizer
