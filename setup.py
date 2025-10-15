19c4bf0d72278d7f1d998effb3039eadb34a6bb4
tang phu thien nhan <demonsnhan@gmail.com>
2025-10-15 22:06:45 +0900

    make SmartWorkcell becomes an custom local package can be install via pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='SmartWorkcell',
+    version='0.0.1',
+    description='Robot arm assistant with VLMs.',
+    license='MIT',
+    packages=['SmartWorkcell'],
+    package_dir={"": "src"},
+    author='TANG PHU THIEN NHAN',
+    author_email='tangptnhan@gmail.com',
+    keywords=['smart workcell'],
+    url='https://github.com/norman-nhan/SmartWorkcell.git'
+)
