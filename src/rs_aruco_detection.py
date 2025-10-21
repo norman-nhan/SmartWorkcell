a5f30fcea25d67521569d551d41f32eeec509665
Tang Phu Thien Nhan <tangptnhan@gmail.com>
2025-10-22 00:46:55 +0900

    modified for demo

--- src/rs_aruco_detection.py
+++ src/rs_aruco_detection.py
@@ -7,13 +7,14 @@ from pysmartworkcell import (
 )
 import pyrealsense2 as rs
 from pyrealsense2 import camera_info
+import cv2
 
 def detect(dict: int, marker_len: float, calib_path: str):
     pkg_root = calib_utils.find_pkg_path()
-    cam_mtx, dist_coeffs = calib_utils.load_camera_intrinsic(calib_path)
-    detectNode = ArucoDetection(
-        dict, cam_matrix=cam_mtx, dist_coeffs=dist_coeffs, marker_length=marker_len
-    )
+    # cam_mtx, dist_coeffs = calib_utils.load_camera_intrinsic(calib_path)
+    # detectNode = ArucoDetection(
+    #     dict, cam_matrix=cam_mtx, dist_coeffs=dist_coeffs, marker_length=marker_len
+    # )
     pipeline, config = rs_utils.initialize_realsense_camera()
     colorizer = rs.colorizer()
     try:
@@ -34,17 +35,33 @@ def detect(dict: int, marker_len: float, calib_path: str):
             color_img = np.asanyarray(color_frame.get_data())
             depth_img = np.asanyarray(colorizer.colorize(depth_frame).get_data())
             depth16 = np.asanyarray(depth_frame.get_data())
+
+            image_concat = np.hstack((color_img, depth_img))
+            cv2.imshow('rgb+depth', image_concat)
+            
+            # Save image and depth
+            key = cv2.waitKey(1)
+            if key == ord('s'):
+                cv2.imwrite('../io/color.png', color_img)
+                np.save('../io/depth.npy', depth16)
+                print(f'Image & Depth are saved.')
+            if key == ord('q'):
+                break
             
             # Detect aruco marker
-            success, ids, T_list = detectNode.estimate_maker_pose_from_frame(color_img)
-            if success:
-                print('Markers found! Compute transformation matrices...')
-                save_path = pkg_root/'config'/'cam2marker.yaml'
-                calib_utils.save_transform_mtx(ids, T_list=T_list, path=save_path)
-                return success, ids, T_list
-
+            # success, ids, T_list = detectNode.estimate_maker_pose_from_frame(color_img)
+            # if success:
+            #     print('Markers found! Compute transformation matrices...')
+            #     save_path = pkg_root/'config'/'cam2marker.yaml'
+            #     calib_utils.save_transform_mtx(ids, T_list=T_list, path=save_path)
+            #     return success, ids, T_list
+      
     except Exception as e:
         raise e
     finally:
         pipeline.stop()
+        cv2.destroyAllWindows()
         print('RealSense camera shutdown successfully!')
+
+if __name__ == '__main__':
+    detect(cv2.aruco.DICT_4X4_50, 0.01, '')
