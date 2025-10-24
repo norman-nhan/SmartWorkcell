fb309f3e23912981d60672e7a11db2b84a795695
shreyan01 <shreyanshukla19@gmail.com>
2025-10-24 21:55:14 +0900

    voice commander update

--- src/voice_command/voice.py
+++ src/voice_command/voice.py
@@ -1,18 +1,25 @@
 from vosk import Model, KaldiRecognizer
-import pyaudio, json 
-import os 
+import sounddevice as sd 
+import queue 
+import json 
 
-def voice_command():
-    model = Model("model")
-    rec = KaldiRecognizer(model, 16000)
+model_en=Model("vosk-model-small-en-us-0.22")
+model_jp=Model("vosk-model-small-ja-0.22")
 
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
-    stream.start_stream()
-    while True:
-        data = stream.read(4096)
-        if len(data) == 0:
-            break
-        if rec.AcceptWaveform(data):
-            result = json.loads(rec.Result()["text"])
-            return result
+q=queue.Queue() 
+
+def callback(indata, frames, time, status):
+    q.put(bytes(indata))
+
+def listen():
+    rec=KaldiRecognizer(model_en, 16000)
+
+    with sd.RawInputStream(samplerate=16000, channels=1, device=0, dtype='int16', frames_per_buffer=8192, callback=callback):
+        print("Listening...")
+        while True:
+            data = q.get()
+            if rec.AcceptWaveform(data): 
+                result = json.loads(rec.Result())
+                text=result.get("text", "")
+                if text:
+                    yield text
